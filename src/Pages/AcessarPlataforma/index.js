475624f1c911bcb06aae67744baf4eb7496b356e
Felipe Catete <83040279+felipe-catete@users.noreply.github.com>
2021-07-05 22:50:12 -0300

    Create index.js

---
+++ src/Pages/AcessarPlataforma/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+function AcessarPlataforma() {
+    return (
+        <div>
+            <h1>Acessar Plataforma</h1>
+        </div>
+    )
+}
+
+export default AcessarPlataforma
