1f25b6935122716485a39018fa1ef94b3c5d7d6b
Felipe Catete <83040279+felipe-catete@users.noreply.github.com>
2021-07-06 11:44:50 -0300

    Add files via upload

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,11 +1,22 @@
 import React from "react";
+import brain from "../../Images/brain.png";
+import "./styles.css";
 
 
 
 function Home() {
     return (
-        <div>
-            <h1>Home (página principal)</h1>
+        <div className="home_main_div">
+            <div className="home_img_div">
+                <img src={brain} />
+            </div>
+            <div className="home_text_div">
+                <h1>Combine estratégias eficazes com resultados concretos para desenvolver as soft skills nos estudantes.</h1>
+                <p>Somos a primeira plataforma a integrar e a sistematizar um conjunto de programas para as escolas trabalharem e mostrarem os resultados do <b>desenvolvimento integral</b> de seus alunos.</p>
+                <button type="button">
+                Fale com um especialista
+                </button>
+            </div>
         </div>
     )
 }
